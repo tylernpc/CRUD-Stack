2cb08195823e8b7ff134e6a42d3fd28b561907f3
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-21 11:51:53 -0500

    Read Updates, and Create Updates here

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ app.listen(3000, function () {
   console.log("listening on 3000");
 });
 
+
+// read operation
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+// read operation
+
+// create operation
+app.post("/names", (req, res) => {
+    console.log('Success');
+})
+// create operation
